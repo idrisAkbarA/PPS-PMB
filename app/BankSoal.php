4cc95cc70aab91553e0f44ac6aa4b6082e622a7b
Idris Akbar Adyusman <idrissmp32@gmail.com>
2020-12-22 22:10:32 +0700

    set some relation

--- app/BankSoal.php
+++ app/BankSoal.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class BankSoal extends Model
 {
-    //
+    public function jurusan()
+    {
+        $this->belongsTo('App\Jurusan');
+    }
+    public function kategori()
+    {
+        $this->belongsTo('App\Kategori');
+    }
 }
--- app/Jurusan.php
+++ app/Jurusan.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Jurusan extends Model
 {
-    //
+    public function ujian()
+    {
+        $this->hasMany('App\Ujian');
+    }
+    public function bank_soal()
+    {
+        $this->hasMany('App\BankSoal');
+    }
 }
