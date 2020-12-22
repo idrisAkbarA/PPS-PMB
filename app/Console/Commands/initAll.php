25ced9e1bcaab196ed73b4fa2db118f3c4ae5145
Idris Akbar Adyusman <idrissmp32@gmail.com>
2020-12-22 23:29:12 +0700

    initAll

---
+++ app/Console/Commands/initAll.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class initAll extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'initAll';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'initialize All';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->call("migrate:fresh");
+        $this->call("initAdmin");
+        $this->call("initJurusan");
+        $this->call("initClnMhs");
+    }
+}
