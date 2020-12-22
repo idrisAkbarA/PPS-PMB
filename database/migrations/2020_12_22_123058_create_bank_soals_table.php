32a678ab6822bf1e49d247de401b84aeb1f6418a
Idris Akbar Adyusman <idrissmp32@gmail.com>
2020-12-22 19:32:38 +0700

    Init some models

--- database/migrations/2020_12_22_123058_create_bank_soals_table.php
+++ database/migrations/2020_12_22_123058_create_bank_soals_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateKategorisTable extends Migration
+class CreateBankSoalsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,8 @@ class CreateKategorisTable extends Migration
      */
     public function up()
     {
-        Schema::create('kategoris', function (Blueprint $table) {
+        Schema::create('bank_soals', function (Blueprint $table) {
             $table->id();
-            $table->string('nama');
-            $table->text('deskripsi');
-            $table->integer('id_jurusan');
             $table->timestamps();
         });
     }
@@ -29,6 +26,6 @@ class CreateKategorisTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('kategoris');
+        Schema::dropIfExists('bank_soals');
     }
 }
